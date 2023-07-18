fb8bef6eb0929775ccc9e1f7fdd2e4309761989c
Víctor Barreto <victorbarretolins@gmail.com>
2023-07-17 22:00:21 -0300

    feat: adicionado header no MarketingLayout

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,7 +1,21 @@
+import Link from "next/link";
+
 interface MarketingLayoutProps {
 	children: React.ReactNode;
 }
 
 export default function MarketingLayout({ children }: MarketingLayoutProps) {
-	return <div className="mx-auto w-full px-4">{children}</div>;
+	return (
+		<div className="mx-auto w-full px-4">
+			<header>
+				<Link href="/">
+					<span>Taxonomy</span>
+				</Link>
+				<div>
+					<Link href="/login">Login</Link>
+				</div>
+			</header>
+			<main>{children}</main>
+		</div>
+	);
 }
