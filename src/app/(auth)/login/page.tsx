587e9ec706fc72aea9f305aa08c602930a3226ab
Víctor Barreto <victorbarretolins@gmail.com>
2023-08-03 20:50:06 -0300

    feat: iniciado página de register

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,15 @@
+import Link from "next/link";
+
+import { Icons } from "@/components/icons";
+
 export default function LoginPage() {
-	return <div className="">LOGIN PAGE</div>;
+	return (
+		<div className="">
+			<p className="px-8 text-center text-sm text-slate-500">
+				<Link href="/register" className="underline hover:text-brand">
+					Don&apos;t have an account? Sign Up
+				</Link>
+			</p>
+		</div>
+	);
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,3 +1,27 @@
+import { Icons } from "@/components/icons";
+import Link from "next/link";
+
 export default function RegisterPage() {
-	return <div className="">REGISTER PAGE</div>;
+	return (
+		<div className="grid h-screen w-screen grid-cols-2 flex-col items-center justify-center">
+			<Link
+				href="/login"
+				className="absolute top-8 right-8 inline-flex items-center justify-center rounded-lg border border-transparent bg-transparent py-2 px-3  text-center text-sm font-medium text-slate-900 hover:border-slate-100 hover:bg-slate-100 focus:z-10 focus:outline-none focus:ring-4 focus:ring-slate-200 dark:border-slate-600 dark:bg-slate-800 dark:text-slate-400 dark:hover:bg-slate-700 dark:hover:text-white dark:focus:ring-slate-700"
+			>
+				Login
+			</Link>
+			<div className="h-full bg-slate-100" />
+			<div className="p-8">
+				<div className="mx-auto flex w-[350px] flex-col justify-center space-y-6">
+					<div className="flex flex-col space-y-2 text-center">
+						<Icons.logo className="mx-auto h-6 w-6" />
+						<h1 className="text-2xl font-bold">Create an account</h1>
+						<p className="text-sm text-slate-500">
+							Enter your email below to create your account
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
